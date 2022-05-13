6a69670f42b847df7a2ea22208e8358ab00c2c63
skku-pdse <100822515+skku-pdse@users.noreply.github.com>
2022-05-13 10:00:05 +0900

    Update PostbioticEvaluationWithDiets.m

--- Scripts/Postbiotic evaluation/PostbioticEvaluationWithDiets.m
+++ Scripts/Postbiotic evaluation/PostbioticEvaluationWithDiets.m
@@ -36,29 +36,8 @@ temp_model = constrainDiets(temp_model,ex_rxns,lb_values);
 temp_model = changeRxnBounds(temp_model,'EX_o2(e)',0,'l');
 temp_model = changeRxnBounds(temp_model,'ATPH',0,'l');
 
-%% Generate crowd positions
-%[modelIrrev, matchRev, irrev2rev, solution, EnzConSamples] = generateCrowdPositions(temp_model,C,biomass,num);
-
-%% Identify best crowding positions with non-zero growth and lactate production
-% NonZero_BiomassCrdInds = find((solution(find(ismember(irrev2rev,find(ismember(model.rxns,biomass)))),:)));
-% L_LactateExchInd = find(ismember(modelIrrev.rxns,{'EX_lac-L(e)_f'}));
-% D_LactateExchInd = find(ismember(modelIrrev.rxns,{'EX_lac-D(e)_f'}));
-% NonZero_L_LactateCrdInds=find((solution(L_LactateExchInd,:)));
-% NonZero_D_LactateCrdInds=find((solution(D_LactateExchInd,:)));
-% NonZero_Biomass_Lactate_CrdInds = intersect(NonZero_BiomassCrdInds,union(NonZero_L_LactateCrdInds,NonZero_D_LactateCrdInds));
-% [Nbins,Edges] = histcounts(log(solution(1,NonZero_Biomass_Lactate_CrdInds)),80);
-% minGR = Edges((find(Nbins == max(Nbins))-2));
-% maxGR = Edges((find(Nbins == max(Nbins))));
-% MinMaxGRIndsOf_NonZero_Biomass_Lactate_CrdInds = [];
-% for h = 1:length(NonZero_Biomass_Lactate_CrdInds)
-%     if log(solution(BiomassInd,NonZero_Biomass_Lactate_CrdInds(h))) >= minGR
-%         if log(solution(BiomassInd,NonZero_Biomass_Lactate_CrdInds(h))) <= maxGR
-%             MinMaxGRIndsOf_NonZero_Biomass_Lactate_CrdInds = [MinMaxGRIndsOf_NonZero_Biomass_Lactate_CrdInds,NonZero_Biomass_Lactate_CrdInds(h)];
-%         end
-%     end
-% end
-% Best_Crd_positions = MinMaxGRIndsOf_NonZero_Biomass_Lactate_CrdInds;
-
+% Generate crowd positions
+% Identify best crowding positions with non-zero growth and lactate production
 [f, v, modelIrrev, Best_Crd_positions, EnzConSamples] = FBAwMC_LAB(temp_model,C,biomass,num);
 
 %% Postbiotic evaluation with model constrained with diet and calculated best crowding positions
